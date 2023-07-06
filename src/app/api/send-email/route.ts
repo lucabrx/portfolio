329bad67276a44c8117f3b56c8bad57ef6e71f71
Luka Brkovic <lukabrkovic@proton.me>
2023-07-06 08:17:13 +0200

    .

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -6,12 +6,13 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
     const body = await request.json();
+    console.log(body);
     
   try {
     const data = await resend.emails.send({
       from: "Work <onboarding@resend.dev>",
       to: ["lukabrkovic@proton.me"],
-      subject: "Hello world",
+      subject: body.email,
       text: 
       `Message from ${body.name} (${body.email}):
         ${body.message}`,
