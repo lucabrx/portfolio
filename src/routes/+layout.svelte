37c11678096f3e03d432ffe39d13d84de22e1a8a
Luka <lukabrkovic@skiff.com>
2023-11-10 13:39:16 +0100

    svelte init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '../app.css';
+	import TailwindIndicator from '$lib/components/TailwindIndicator.svelte';
+	import Navbar from '$lib/components/Navbar.svelte';
+	import { dev } from '$app/environment';
+</script>
+
+<Navbar />
+<slot />
+{#if dev}
+	<TailwindIndicator />
+{/if}
