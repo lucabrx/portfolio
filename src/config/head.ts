f04d1e5c3f2d4a5371c355588a1de180981500cc
Luka <lukabrkovic@skiff.com>
2023-11-10 12:58:31 +0100

    removed

--- src/config/head.ts
+++
@@ -1,9 +0,0 @@
-interface HeadConfig {
-  title: string
-  description: string
-}
-
-export const headConfig: HeadConfig = {
-  title: "Luka Brkovic",
-  description: "Luka Brkovic - Web Developer, Portfolio, Blog, Resume, Contact, About",
-}
